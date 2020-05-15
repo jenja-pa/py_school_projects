48b43d114277efacaf9839f43126c0707657c8fa
jenja.pa <jenja.pa@gmail.com>
2020-05-15 16:02:35 +0300

    first commit start files

---
+++ 03-fight/fight.py
@@ -0,0 +1,24 @@
+from tkinter import *
+ 
+app = Tk()
+app.title = "Sea War"
+
+gFrame = Frame(master=  app,width=500, height=500, bg="white")
+ 
+# e = Entry(width=20)
+# b = Button(text="Преобразовать")
+# l = Label(bg='black', fg='white', width=20)
+ 
+# def strToSortlist(event):
+#     s = e.get()
+#     s = s.split()
+#     s.sort()
+#     l['text'] = ' '.join(s)
+ 
+# b.bind('<Button-1>', strToSortlist)
+ 
+# e.pack()
+# b.pack()
+# l.pack()
+
+app.mainloop()
