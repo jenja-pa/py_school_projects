c07fa5c58cc03d86c6f378d2c3bc9415f98944f3
jenja.pa <jenja.pa@gmail.com>
2020-05-15 17:03:28 +0300

    Реалізовано виконання пострілу при натисканні space

--- 03-fight/fight.py
+++ 03-fight/fight.py
@@ -4,21 +4,28 @@ import time
 tk = Tk()
 tk.title = "Sea War"
 
-def draw_gun(cnv, c_x):
-  cnv.create_rectangle(c_x-60, 480, c_x+60, 500, fill="black")
-  cnv.create_rectangle(c_x-10, 460, c_x+10, 480, fill="black")
-
-
 canvas = Canvas(tk, width=500, height=500)
 canvas.pack()
 
+def draw_gun(c_x):
+  canvas.create_rectangle(c_x-60, 480, c_x+60, 500, fill="black")
+  canvas.create_rectangle(c_x-10, 460, c_x+10, 480, fill="black")
+
+def ball(event):
+  global b
+  canvas.delete(b)
+  b=canvas.create_oval(240, 460,260,480, fill="gray")
+canvas.bind_all("<space>", ball)
+
 ship_image = PhotoImage(file = "ship.gif")
 s = canvas.create_image(500, 0, anchor =NW, image = ship_image)
+b = 0
 
-draw_gun(canvas, 250)
+draw_gun(250)
 
 for y in range(200):
   canvas.move(s, -3, 0)
+  canvas.move(b, 0, -5)
   tk.update()
   time.sleep(0.02)
 
