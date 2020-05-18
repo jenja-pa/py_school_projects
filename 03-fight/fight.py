75b5afe78450014ed7704e54c547034be2b2ff2f
jenja-pa <jenja.pa@gmail.com>
2020-05-18 12:19:51 +0300

    Add output message about count of shots

--- 03-fight/fight.py
+++ 03-fight/fight.py
@@ -1,6 +1,6 @@
-from  tkinter import *
 import time
- 
+from tkinter import *
+
 tk = Tk()
 tk.title = "Sea War"
 
@@ -8,28 +8,36 @@ canvas = Canvas(tk, width=500, height=500)
 canvas.pack()
 
 def draw_gun(c_x):
-  canvas.create_rectangle(c_x-60, 480, c_x+60, 500, fill="black")
-  canvas.create_rectangle(c_x-10, 460, c_x+10, 480, fill="black")
+    canvas.create_rectangle(c_x-60, 480, c_x+60, 500, fill="black")
+    canvas.create_rectangle(c_x-10, 460, c_x+10, 480, fill="black")
 
+b = 0
+shots = 0
 def ball(event):
-  global b, shots
-  canvas.delete(b)
-  b=canvas.create_oval(240, 460,260,480, fill="gray")
+    global b, shots
+    # Витираємо попередній напис
+    canvas.create_text(420, 100, text="кількість пострілів: " + str(shots), fill="#F0F0ED")
+    if shots < 10:
+        shots = shots + 1
+        canvas.create_text(420, 100, text="кількість пострілів: " + str(shots))            
+        canvas.delete(b)
+        b=canvas.create_oval(240, 460,260,480, fill="gray")
+    else:
+        canvas.create_text(420, 100, text="Ядра закінчилися!")            
+
 canvas.bind_all("<space>", ball)
 
 ship_image = PhotoImage(file = "ship.gif")
 s = canvas.create_image(500, 0, anchor =NW, image = ship_image)
-b = 0
-
-shots = 0
 
 draw_gun(250)
 
+# Основний процес гри
 for y in range(200):
-  canvas.move(s, -3, 0)
-  canvas.move(b, 0, -5)
-  tk.update()
-  time.sleep(0.02)
+    canvas.move(s, -3, 0)
+    canvas.move(b, 0, -5)
+    tk.update()
+    time.sleep(0.02)
 
 
 # gFrame = Frame(master=  tk,width=500, height=500, bg="white")
